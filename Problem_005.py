01d8888c3c8ca01a6e10e45106ce16dbeccfa48b
Ariel Tynan <90798242+arieltynan@users.noreply.github.com>
2022-01-23 15:07:16 -0700

    Add files via upload

---
+++ Problem_005.py
@@ -0,0 +1,13 @@
+# Smallest multiple
+# smallest positive num evenly divisible by 1 to 20?
+
+
+
+# only need to consider 11-20
+# multiply primes 11, 13, 17, 19
+# find min number of other prime factors 
+# 2^4, 3^2, 5^1, 7^1
+# Multiply all together
+
+min = 11*13*17*19*16*9*5*7
+print(min)
